0dab98a5f955a5d1b67d6612b5e429d9212cc45c
Justin Morris <justin@pixelbloom.com>
2011-07-30 23:03:04 +1000

    Added build URL for project

--- app/apps/project/urls.py
+++ app/apps/project/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     url(r'^project/new/$', create, name='create'),
     url(r'^$', index, name='index'),
     url(r'^project/(?P<project_name_slug>[0-9A-Za-z-]+)/$', show, name='show'),
+    url(r'^project/(?P<project_name_slug>[0-9A-Za-z-]+)/build/$', build, name='build'),
     url(r'^project/(?P<project_name_slug>[0-9A-Za-z-]+)/config/$', config, name='config'),
     url(r'^project/(?P<project_name_slug>[0-9A-Za-z-]+)/delete/$', delete, name='delete'),
 )
