eb94497f2cff4e3a190204ea506fafd2fd56f3d9
Michael Morris <mtcmorris@gmail.com>
2011-07-31 13:13:18 +1000

    Removed references to running from build models

--- app/apps/build/models.py
+++ app/apps/build/models.py
@@ -5,11 +5,7 @@ from django.dispatch import receiver
 from django.conf import settings
 
 from ..project.models import Project
-from ..worker.tasks import exec_build
-from ..worker.relay import BuildWebsocketRelay
 import datetime
-import multiprocessing
-
 
 class Build(models.Model):
     """(Project description)"""
@@ -55,21 +51,21 @@ class Step(models.Model):
     
     state = models.CharField(blank=True, max_length=1, default="a", choices=STATE_CHOICES)
 
-@receiver(post_save, sender=Build)
-def trigger_build(sender, instance, **kwargs):
-    async_build = exec_build.delay(build=instance)
-    # running_builds = Build.objects.filter(state='b')
-    # if not len(running_builds):
-    #     build = instance
-    #     queue = multiprocessing.Queue()
-    #     buildProcess = multiprocessing.Process(
-    #         target=BuildWebsocketRelay,
-    #         name='buildwebsocketrelay',
-    #         kwargs={
-    #             'port':settings.BUILDRELAY_SOCKET_PORT,
-    #             'path': build.project.get_absolute_url(),
-    #             'queue':queue,
-    #         }
-    #     )
-    #     buildProcess.start()
-    #     # async_build = exec_build.delay(build=build)
+# @receiver(post_save, sender=Build)
+# def trigger_build(sender, instance, **kwargs):
+#     async_build = exec_build.delay(build=instance)
+#     # running_builds = Build.objects.filter(state='b')
+#     # if not len(running_builds):
+#     #     build = instance
+#     #     queue = multiprocessing.Queue()
+#     #     buildProcess = multiprocessing.Process(
+#     #         target=BuildWebsocketRelay,
+#     #         name='buildwebsocketrelay',
+#     #         kwargs={
+#     #             'port':settings.BUILDRELAY_SOCKET_PORT,
+#     #             'path': build.project.get_absolute_url(),
+#     #             'queue':queue,
+#     #         }
+#     #     )
+#     #     buildProcess.start()
+#     #     # async_build = exec_build.delay(build=build)
