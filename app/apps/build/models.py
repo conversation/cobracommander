8fcc95429655f0b626b76267431e5244bcac013f
Justin Morris <justin@pixelbloom.com>
2011-07-31 10:40:40 +1000

    Remove m2m from project->build and replace with FK from build to project. We can reverse this much easier later on and it make life much easier for creating builds.

--- app/apps/build/models.py
+++ app/apps/build/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.core.urlresolvers import reverse
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.conf import settings
+
+from ..project.models import Project
+from ..worker.tasks import exec_build
+from ..worker.relay import BuildWebsocketRelay
 import datetime
+import multiprocessing
 
 
 class Build(models.Model):
@@ -13,11 +21,12 @@ class Build(models.Model):
         ("d", "fail",),
     )
     
+    project = models.ForeignKey(Project)
     ref = models.CharField(blank=False, max_length=100, db_index=True)
     state = models.CharField(blank=True, max_length=1, default="a", choices=STATE_CHOICES)
     created_datetime = models.DateTimeField(blank=True, default=datetime.datetime.now)
-    start_datetime = models.DateTimeField(blank=True)
-    end_datetime = models.DateTimeField(blank=True)
+    start_datetime = models.DateTimeField(blank=True, null=True)
+    end_datetime = models.DateTimeField(blank=True, null=True)
     
     def __unicode__(self):
         return u"%s" % (self.ref)
@@ -27,3 +36,22 @@ class Build(models.Model):
         return ('build:show', (), {
             'build_ref':self.ref
         })
+
+@receiver(post_save, sender=Build)
+def trigger_build(sender, instance, **kwargs):
+    async_build = exec_build.delay(build=instance)
+    # running_builds = Build.objects.filter(state='b')
+    # if not len(running_builds):
+    #     build = instance
+    #     queue = multiprocessing.Queue()
+    #     buildProcess = multiprocessing.Process(
+    #         target=BuildWebsocketRelay,
+    #         name='buildwebsocketrelay',
+    #         kwargs={
+    #             'port':settings.BUILDRELAY_SOCKET_PORT,
+    #             'path': build.project.get_absolute_url(),
+    #             'queue':queue,
+    #         }
+    #     )
+    #     buildProcess.start()
+    #     # async_build = exec_build.delay(build=build)
--- app/apps/project/models.py
+++ app/apps/project/models.py
@@ -3,18 +3,16 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 import datetime
 
-from ..build.models import Build
-
 
 class Project(models.Model):
     """(Project description)"""
     
     name = models.CharField(blank=False, max_length=100)
     name_slug = models.SlugField(blank=False, db_index=True)
-    repo_url = models.URLField(blank=True, verify_exists=True, db_index=True)
+    repo_url = models.URLField(blank=False, verify_exists=True, db_index=True)
+    repo_clone_url = models.CharField(blank=False, max_length=255)
     branch = models.CharField(blank=False, max_length=100, default="master")
     description = models.TextField(blank=True)
-    builds = models.ManyToManyField(Build, blank=True, null=True)
     created_datetime = models.DateTimeField(blank=False, default=datetime.datetime.now)
     
     class Meta:
