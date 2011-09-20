9fae3d84bc9c97e4b68cff0d2f1413c529947261
Justin Morris <justin@pixelbloom.com>
2011-09-20 18:05:59 +1000

    Move git.py

--- app/apps/worker/runner/git.py
+++ app/apps/worker/runner/git.py
@@ -1,19 +1,18 @@
 from django.conf import settings
 import os, subprocess
 
-
 # TODO: replace with dulwich or somthing less completely crap
 class Git(object):
     """ Shell out and run git commands from the build root """
     def __init__(self, path):
         super(Git, self).__init__()
         self.path = path
-    
+
     def _check_cmd_path_prefix(self):
         self.cmd_path_prefix = ""
         if os.path.exists(self.path):
             self.cmd_path_prefix ='cd "%s" && ' % self.path
-    
+
     def run(self, command):
         self._check_cmd_path_prefix()
         cmd = "%s%s" % (self.cmd_path_prefix, command)
