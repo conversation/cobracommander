bfa508e96977e3aa444f07193d7ef9030ad26455
Justin Morris <justin@pixelbloom.com>
2011-09-20 18:07:31 +1000

    Added step start and end times.

--- app/apps/build/models.py
+++ app/apps/build/models.py
@@ -15,17 +15,17 @@ class Build(models.Model):
         ("c", "pass",),
         ("d", "fail",),
     )
-    
+
     target = models.ForeignKey(Target)
     state = models.CharField(blank=True, max_length=1, default="a", choices=STATE_CHOICES)
     created_datetime = models.DateTimeField(blank=True, default=datetime.datetime.now)
     start_datetime = models.DateTimeField(blank=True, null=True)
     end_datetime = models.DateTimeField(blank=True, null=True)
     log = models.TextField(blank=True)
-    
+
     def __unicode__(self):
         return u"%s" % (self.target)
-    
+
     @models.permalink
     def get_absolute_url(self):
         # TODO: LOL, fix this!
@@ -37,19 +37,21 @@ class Build(models.Model):
 
 class Step(models.Model):
     """Sup"""
-    
+
     STATE_CHOICES = (
         ("a", "pending",),
         ("b", "running",),
         ("c", "pass",),
         ("d", "fail",),
     )
-    
+
     build = models.ForeignKey(Build)
     command = models.CharField(blank=False, max_length=255)
     created_datetime = models.DateTimeField(blank=True, default=datetime.datetime.now)
+    start_datetime = models.DateTimeField(blank=True, null=True)
+    end_datetime = models.DateTimeField(blank=True, null=True)
     output = models.TextField(blank=True)
     state = models.CharField(blank=True, max_length=1, default="a", choices=STATE_CHOICES)
-    
+
     def __unicode__(self):
         return u"%s" % (self.command)
